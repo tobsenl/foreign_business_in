3b1e56d57f880c56978401f6d0ea723116b6b243
tobsenl <tobsenl@hotmail.com>
2013-11-05 14:12:45 +0800

    权限系统测试完毕.正常可用

--- src/main/java/cn/com/jnpc/foreign/controller/LoginController.java
+++ src/main/java/cn/com/jnpc/foreign/controller/LoginController.java
@@ -1,5 +1,7 @@
 package cn.com.jnpc.foreign.controller;
 
+import java.util.Properties;
+import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,52 +12,69 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import cn.com.jnpc.ems.common.EMSUtils;
 import cn.com.jnpc.ems.dto.User;
 import cn.com.jnpc.foreign.po.Users;
+import cn.com.jnpc.foreign.utils.Right;
 import cn.com.jnpc.foreign.utils.Untils;
 
 @Controller
-public class LoginController{
+public class LoginController {
     @RequestMapping(value = "/login.html")
-    public String login(Users user,HttpServletRequest request,Model model){
-	User use=null;
-	boolean flag=false;
+    public String login(Users user, HttpServletRequest request, Model model) {
+	User use = null;
+	boolean flag = false;
 	try {
-	    if(Untils.getSessionP(request, "user") != null){
-		flag=true;
-	    }else{
-    	    if(user!=null && Untils.NotNull(user.getUserid()) && Untils.NotNull(user.getPasswd())){
-    		flag= Untils.verifyUser( user.getUserid(),user.getPasswd());
-    		if (flag) {
-    			use=(User)EMSUtils.findUserByAccount(user.getUserid());
-    			boolean check=Untils.setSessionP(request, "user", (Object)use);
-    			if(check){
-    			    model.addAttribute("user",use);
-    			}else{
-    			    model.addAttribute("error","系统错误请联系IT部门");
-    			}
-    		}else{
-    		    model.addAttribute("error","用户名或者密码错误");
-    		}
-    	    }else{
-    		model.addAttribute("error","请填写用户名密码!");
-    	    }
+	    if (Untils.getSessionP(request, "user") != null) {
+		flag = true;
+	    } else {
+		if (user != null && Untils.NotNull(user.getUserid())
+			&& Untils.NotNull(user.getPasswd())) {
+		    flag = Untils
+			    .verifyUser(user.getUserid(), user.getPasswd());
+		    if (flag) {
+			Properties prop = null;
+			prop = Untils.loadProperties("/ad_load.properties", 7);
+			String rolecode = prop.getProperty("rolecode");
+			Right right = new Right();
+			use = (User) EMSUtils.findUserByAccount(user
+				.getUserid());
+			int check=0;
+			if(use!=null){
+			check = Untils.setSessionP(request, "user",
+				(Object) use)==false?(check+1):(check+0);
+			check = Untils.setSessionP(request, "vec",
+				right.getRights(use.getAccount(), rolecode))==false?(check+1):(check+0);
+			check = Untils.setSessionP(request, "prop",prop)==false?(check+1):(check+0);
+			}
+			if (check <= 0) {
+			    model.addAttribute("user", use);
+			} else {
+			    model.addAttribute("error", "系统错误请联系IT部门");
+			}
+		    } else {
+			model.addAttribute("error", "用户名或者密码错误");
+		    }
+		} else {
+		    model.addAttribute("error", "请填写用户名密码!");
+		}
 	    }
 	} catch (Exception e) {
-	    System.out.println(e.getCause()); 
+	    System.out.println(e.getCause());
 	}
-	if(flag){
-	    return "index/index";	    
-	}else{
-	    return "login";//返回路径的问题.
+	if (flag) {
+	    return "index/index";
+	} else {
+	    return "login";// 返回路径的问题.
 	}
     }
+
     @RequestMapping(value = "/login.do")
-    public String login(HttpServletRequest request,Model model){
+    public String login(HttpServletRequest request, Model model) {
 	return "index/index";
     }
+
     @RequestMapping(value = "/loginout.html")
-    public String loginout(HttpServletRequest request,Model model){
-	Untils.moveOutSessionP(request,"user");
+    public String loginout(HttpServletRequest request, Model model) {
+	Untils.moveOutSessionP(request, "user");
 	return "/index";
     }
-    
+
 }
